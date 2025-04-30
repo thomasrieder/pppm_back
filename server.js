bbab89807f8e4499966b6f1aa5f673e99af3605d
thomasrieder <thom.rieder@gmail.com>
2025-04-30 06:54:29 +0200

    vasy

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
+app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello from our server!')
+    res.send('Hello from toto!')
 });
 
 app.listen(port, () => {
